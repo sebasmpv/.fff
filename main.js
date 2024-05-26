2bc12e93a14f045d05e24e1617ae287055e3ee7c
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-25 22:08:44 -0500

    𝙑𝙀𝙂𝙀𝙏𝙏𝘼 𝘽𝙊𝙏 🤠💫

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-procss.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js' 
 import './plugins/_content.js'
 import { createRequire } from 'module'
